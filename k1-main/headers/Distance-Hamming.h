6ceff20f9dce0a6fb7f6088253a50c3b1cc91648
Augustin <50630994+Gugus5651@users.noreply.github.com>
2026-05-05 12:01:20 +0200

    Delete k1-main directory

--- k1-main/headers/Distance-Hamming.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISTANCE_HAMMING_H
-#define DISTANCE_HAMMING_H
-
-float Distance_hamming(const char *mot1, const char *mot2);
-
-#endif
--- k1-main/headers/Distance-Levenshtein.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISTANCE_LEVENSHTEIN_H
-#define DISTANCE_LEVENSHTEIN_H
-
-float Distance_levenshtein(const char *mot1, const char *mot2);
-
-#endif
